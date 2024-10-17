7ea85ba302a1584cb42038a80a66d8fb2900ee73
icaroccaetano <icaroccaetano@gmail.com>
2024-10-17 20:45:12 -0300

    first commit

---
+++ bot.py
@@ -0,0 +1,28 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from datetime import datetime
+
+def main():
+    while True:
+        driver = webdriver.Chrome()
+        driver.implicitly_wait(60)
+        driver.minimize_window()
+        driver.get("https:/unificada.com")
+        layer = driver.find_element(By.XPATH, "//*[@id='root']/div/main/div/div[2]/div[2]/div[1]/div[1]")
+        driver.implicitly_wait(10)
+        button = layer.find_element(By.XPATH, ".//button")  
+        button.click()
+        WebDriverWait(driver, 10).until(EC.alert_is_present())
+        msg = driver.switch_to.alert.text
+        agora = datetime.now()
+
+        # Formata a data e o horário
+        data_formatada = agora.strftime("%Y-%m-%d %H:%M:%S")
+        print(f"{data_formatada}  - {driver.switch_to.alert.text}")
+        driver.switch_to.alert.accept()
+        driver.close()
+
+main()
